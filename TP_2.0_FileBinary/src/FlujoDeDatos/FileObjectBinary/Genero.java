3a2724f59f99ad57b615e386fdeceb7b2289d190
KennyKall <102248168+KennyKall@users.noreply.github.com>
2023-02-02 15:55:18 -0300

    Add files via upload

---
+++ TP_2.0_FileBinary/src/FlujoDeDatos/FileObjectBinary/Genero.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FlujoDeDatos.FileObjectBinary;
+
+/**
+ *
+ * @author PC ARI Central
+ */
+public enum Genero {
+    Masculino,
+    Femenino,
+    NoBinario,
+    SG;
+
+}
