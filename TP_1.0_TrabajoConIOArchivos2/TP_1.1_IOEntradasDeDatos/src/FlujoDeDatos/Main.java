69e3451fa5b42c33a043b389d34d0491dc3febba
KennyKall <102248168+KennyKall@users.noreply.github.com>
2023-02-02 11:23:16 -0300

    Add files via upload

---
+++ TP_1.0_TrabajoConIOArchivos2/TP_1.1_IOEntradasDeDatos/src/FlujoDeDatos/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Clase main donde se ejecutan los metodos de "sistema.java".
+ */
+package FlujoDeDatos;
+
+/**
+ *
+ * @author Mendez Agustín 6°1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sistema s = new Sistema();
+        
+        s.cuentas();
+    }
+    
+}
